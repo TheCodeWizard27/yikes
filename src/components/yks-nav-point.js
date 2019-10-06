1c2790c3bab170c119b79501ecf71df04b244f1f
TheCodeWizard27 <benny.schaerer@gmail.com>
2019-10-06 18:32:39 +0200

    Added Scroll To Navigation to Yks-Nav

--- src/components/yks-nav-point.js
+++ src/components/yks-nav-point.js
@@ -2,10 +2,23 @@ import React from 'react';
 import '../styles/yks-nav-point.css';
 
 class YksNavPoint extends React.Component {
+    constructor(props) {
+        super(props);
+
+        console.log(`${props.title} got called.`)
+
+        this._ref = React.createRef();
+
+        if(this.props.ref)
+            this.props.ref(this._ref.current);
+    }
+
     render() {
         return (
-            <div className="yks-nav-point-container">
-                { this.props.children }
+            <div ref={this._ref} className="yks-nav-point-container">
+                <div className="yks-nav-point-content">
+                    {this.props.children}
+                </div>
             </div>
         );
     }
--- src/components/yks-nav.js
+++ src/components/yks-nav.js
@@ -2,21 +2,47 @@ import React from 'react';
 import '../styles/yks-nav.css'
 
 class YksNav extends React.Component {
-    
+
+    constructor(props){
+        super(props);
+
+        this._refMap = new Map();
+    }
+
     render() {
 
-        let points = this.props.children.map((child, i) => <div key={child.props.title}>{child.props.title}</div>);
+        this._refMap.clear();
+        // Clone Children and add references to them.
+        let children = React.Children.map(this.props.children, child => {
+            return React.cloneElement(child, {
+                ref: (ref) => this._refMap.set(child.props.title, ref)
+            });
+        });
+
+        let points = children.map((child) => (
+            <button className="yks-nav-point"
+                key={child.props.title}
+                onClick={(e) => this.scrollToPoint(child)}>
+                {child.props.title}
+            </button>
+        ));
 
         return (
             <div className="yks-nav-container">
                 <div className="yks-nav-points-container">
                     <div className="yks-nav-points">
-                        { points }
+                        {points}
                     </div>
                 </div>
-                { this.props.children }
+                {children}
             </div>
         );
+    }
+
+    scrollToPoint(point) {
+        // Scroll point into view.
+        let ref = this._refMap.get(point.props.title)._ref.current;
+        ref.scrollIntoView({block: "end", behavior: "smooth"});
     }
 }
 
