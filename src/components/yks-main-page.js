1b631e0c7d3154d46440444fcf3dde6f6cf0298f
TheCodeWizard27 <benny.schaerer@gmail.com>
2019-11-03 18:03:34 +0100

    Fixed merging errors.

--- src/components/yks-main-page.js
+++ src/components/yks-main-page.js
@@ -9,84 +9,92 @@ import YksPortrait from './yks-portrait';
 class YksMainPage extends React.Component {
     componentWillMount() {
         this.setState({
-            imagesSrc: 
+            imagesSrc:
                 [
-                    {file: "P1060027", text: "P1060027"},
-                    {file: "P1060031", text: "P1060031"},
-                    {file: "P1060037", text: "P1060037"},
-                    {file: "P1060036", text: "P1060036"},
-                    {file: "P1060039", text: "P1060039"},
-                    {file: "P1060040", text: "P1060040"},
-                    {file: "P1060044", text: "P1060044"},
-                    {file: "P1060046", text: "P1060046"},
-                    {file: "P1060049", text: "P1060049"},
-                    {file: "P1060051", text: "P1060051"}
+                    { file: "P1060027", text: "P1060027" },
+                    { file: "P1060031", text: "P1060031" },
+                    { file: "P1060037", text: "P1060037" },
+                    { file: "P1060036", text: "P1060036" },
+                    { file: "P1060039", text: "P1060039" },
+                    { file: "P1060040", text: "P1060040" },
+                    { file: "P1060044", text: "P1060044" },
+                    { file: "P1060046", text: "P1060046" },
+                    { file: "P1060049", text: "P1060049" },
+                    { file: "P1060051", text: "P1060051" }
                 ]
         });
     }
 
-        render() {
-            const gallery =
-                this.state.imagesSrc.map((image, i) => {
-                    return (
-                        <div className="pic-container">
-                            <div className="pic-text">
-                                <p>{image["text"]}</p>
-                            </div>
-                            <img className="gallery-pic" src={"/pictures/" + image["file"] + ".jpg"}></img>
+    render() {
+        const gallery =
+            this.state.imagesSrc.map((image, i) => {
+                return (
+                    <div className="pic-container">
+                        <div className="pic-text">
+                            <p>{image["text"]}</p>
                         </div>
-                    );
-                });
+                        <img className="gallery-pic" src={"/pictures/" + image["file"] + ".jpg"}></img>
+                    </div>
+                );
+            });
 
-            return (
-                <YksNav>
-                    <YksNavPoint title="Start">
-                        <div className="flex-column fx-center fill-page">
-                            <div id="title-container">
-                                <div className="flex-row fx-center">
-                                    <h1 id="main-title">Yikes</h1>
-                                </div>
+        return (
+            <YksNav>
+                <YksNavPoint title="Start">
+                    <div className="flex-column fx-center fill-page">
+                        <div id="title-container">
+                            <div className="flex-row fx-center">
+                                <h1 className="main-title">Yikes</h1>
                             </div>
                         </div>
-                    </YksNavPoint>
-                    <YksNavPoint title="Fotos">
-                        <div className="fill-min-page">
-                            <div className="gallery">
-                                {gallery}
-                            </div>
+                    </div>
+                </YksNavPoint>
+                <YksNavPoint title="Fotos">
+                    <div className="fill-min-page">
+                        <div className="gallery">
+                            {gallery}
+                        </div>
 
+                    </div>
+                </YksNavPoint>
+                <YksNavPoint title="Bär Grillt">
+                    <div className="flex fill-page">Bär grillt Page</div>
+                </YksNavPoint>
+                <YksNavPoint title="Livestream">
+                    <div className="flex fill-page yks-card livestream">
+                        <div className="yks-title">Live Stream</div>
+                        <div>Enjoy the 24/7 Stream of nasa doing random interesting stuff. Even thought this is a livestream note that it is not indeed live but just restreaming videos already released.</div>
+                        <div className="livestream-container">
+                            <iframe title="livestream" width="1280" height="720" src="https://www.youtube.com/embed/21X5lGlDOfg" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
                         </div>
-                    </YksNavPoint>
-                    <YksNavPoint title="Bär Grillt">
-                        <div className="flex fill-page">Bär grillt Page</div>
-                    </YksNavPoint>
-                    <YksNavPoint title="Livestream">
-                        <div className="flex fill-page yks-card livestream">
-                            <div className="yks-title">Live Stream</div>
-                            <div>Enjoy the 24/7 Stream of nasa doing random interesting stuff. Even thought this is a livestream note that it is not indeed live but just restreaming videos already released.</div>
-                            <div className="livestream-container">
-                                <iframe title="livestream" width="1280" height="720" src="https://www.youtube.com/embed/21X5lGlDOfg" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                    </div>
+                </YksNavPoint>
+                <YksNavPoint title="Autoren">
+                    <div className="flex fx-center fill-min-page">
+                        <div className="flex fx-wrap full-width fx-space-evenly">
+                            <div className="yks-portrait-container yks-card">
+                                <YksPortrait imageSrc="https://avatars0.githubusercontent.com/u/36076364?s=400&v=4" />
+                                <div>Benny Schärer</div>
+                                <button className="yks-button"><Link to={{
+                                    pathname: '/autor',
+                                    state: { names: ['benny'] }
+                                }}>Mehr...</Link></button>
+                            </div>
+                            <div className="yks-portrait-container yks-card">
+                                <YksPortrait imageSrc="https://pbs.twimg.com/profile_images/1072461276805255168/q4hOFV7K_400x400.jpg" />
+                                <div>Sophia Menger</div>
+                                <button className="yks-button"><Link to={{
+                                    pathname: '/autor',
+                                    state: { names: ['sophia'] }
+                                }}>Mehr...</Link></button>
                             </div>
                         </div>
-                    </YksNavPoint>
-                    <YksNavPoint title="Autoren">
-                        <div className="flex fx-center fill-min-page">
-                            <div className="flex fx-wrap full-width fx-space-evenly">
-                                <YksPortrait imageSrc="https://avatars0.githubusercontent.com/u/36076364?s=400&v=4">
-                                    <div>Benny Schärer</div>
-                                    <button className="yks-button"><Link to="/autor">Mehr...</Link></button>
-                                </YksPortrait>
-                                <YksPortrait imageSrc="">
-                                    <div>Sophia Menger</div>
-                                    <button className="yks-button"><Link to="/autor">Mehr...</Link></button>
-                                </YksPortrait>
-                            </div>
-                        </div>
-                    </YksNavPoint>
-                </YksNav>
-            );
-        }
-
+                    </div>
+                </YksNavPoint>
+            </YksNav >
+        );
     }
 
-    export default YksMainPage;
+}
+
+export default YksMainPage;
